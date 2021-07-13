2213ba0ce39d541772570f8cd5c1ea1032afb122
Jiyoon Kim <jkim@bronxvilleschool.org>
2021-07-13 11:15:20 -0400

    first draft

--- ds/maze/Knights.java
+++ ds/maze/Knights.java
@@ -11,49 +11,45 @@ public class Knights{
     private int rows = 5;
     private int cols = 5;
     private int size=5;
+
+    //magic java code to do stuff i had to look up anyway
     private String clearScreen="[0;0H\n";
-
-    private void delay(int n)
-    {
-	try
-	    {Thread.sleep(n);}
-	catch(InterruptedException e)
-	    {}
-
+    private void delay(int n){
+	     try {Thread.sleep(n);}
+	     catch(InterruptedException e)
+	      {}
     }
 
-
     public Knights(int size){
-	rows = size;
-	cols = size;
-	this.size=size;
-	int row,col;
+	     rows = size;
+	     cols = size;
+	     this.size=size;
+	     int row,col;
 
 	// Notice that the board is 4 bigger in both directions with 0's
 	// in the middle and a 2 row/col border of -1.
 	// Why are we doing this?
-	board = new int[cols+4][rows+4];
-	for (row = 0; row < rows+4; row++){
-	    for (col = 0; col < cols+4 ; col++){
-		board[col][row] = -1;
-	    }
-	}
-	for (row = 2; row < rows+2; row++){
-	    for (col = 2; col < cols+2 ; col++){
-		board[col][row] = 0;
-	    }
-	}
-
-
+  //similar to our maze, the -1 sets up a "wall" to avoid an IOOB error
+	     board = new int[cols+4][rows+4];
+	     for (row = 0; row < rows+4; row++){
+	       for (col = 0; col < cols+4 ; col++){
+		         board[col][row] = -1;
+	        }
+	     }
+	     for (row = 2; row < rows+2; row++){
+	        for (col = 2; col < cols+2 ; col++){
+		          board[col][row] = 0;
+	        }
+	     }
     }
 
     public String toString(){
-	int row,col;
-	int value;
-	String result = "";
-	for (row = 0; row < rows+4; row++){
-	    for (col = 0; col < cols+4; col++){
-		value = board[col][row];
+	     int row,col;
+	     int value;
+	     String result = "";
+	     for (row = 0; row < rows+4; row++){
+	     for (col = 0; col < cols+4; col++){
+		   value = board[col][row];
 
 		// Why do we have this if as opposed to
 		// just adding the next value to the string?
@@ -69,16 +65,13 @@ public class Knights{
     }
 
     public boolean solve(int col,int row, int count){
-	boolean solved = false;
-
-
-	// This should return true when we've solved the problem
-	// What should CHANGETHIS be?
-	// in the maze we knew we were done when we found the exit
-	// here, when do we know when we're done?
-	// HINT: you have an nxn board and are done when you've visited
-	// every board location
-	if (count>CHANGETHIS){
+	     boolean solved = false;
+	// This should return true when we've solved the problem. What should CHANGETHIS be?
+  //A: (rows * cols)
+	// in the maze we knew we were done when we found the exit. here, when do we know when we're done?
+  //A: when we've visited all the spaces
+	// HINT: you have an nxn board and are done when you've visited every board location
+	if (count>=(rows*cols)){
 	    System.out.println(this);
 	    return true;
 	}
@@ -88,14 +81,14 @@ public class Knights{
 	// change CHANGETHIS to the appropriate boolean
 	// HINT: we are tracking our moves in the board
 	// and also built that border of -1 values.
-	if (CHANGETHIS){
+	if (board[row][col] == -1 //trying to go outside of the board therefore return false
+      || board[row][col]!=0){ //if you've visited before; not viable space therefore return false
 	    return false;
 	}
 
-
 	// what do we put into the board
-	// Change CHANGETHIS
-	board[col][row]=CHANGETHIS;
+	board[col][row]=count;
+  count++;
 
 	delay(50);
 	System.out.println(clearScreen+this);
@@ -110,6 +103,23 @@ public class Knights{
 
 
 
+  // RECURSIVE CALLS
+  // try all the spaces we can go to
+  solved = solve(col+1,row+2,count);
+  if (!solved)
+      solved = solve(col-1,row+2,count);
+  if (!solved)
+      solved = solve(col+1,row-2,count);
+  if (!solved)
+      solved = solve(col-1,row-2,count);
+  if (!solved)
+      solved = solve(col+2,row-1,count);
+  if (!solved)
+      solved = solve(col+2,row+1,count);
+  if (!solved)
+      solved = solve(col-2,row-1,count);
+  if (!solved)
+      solved = solve(col-2,row+1,count);
 
 	// Here we unset where we were for the backtracking
 
