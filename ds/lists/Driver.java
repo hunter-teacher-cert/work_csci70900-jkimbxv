e31c49cd942e2902b9265edd79f7d86b1e537012
JiyoonKim10 <>
2021-07-14 17:59:25 +0000

    done

--- ds/lists/Driver.java
+++ ds/lists/Driver.java
@@ -17,17 +17,34 @@ public class Driver{
 
 	System.out.println(n.getNext());
 	n.getNext().setNext(new Node("Steph"));
-	System.out.println(n2.getNext());
+	System.out.println(n.getNext());
 	System.out.println(n.getNext().getNext());
 
-	Node L;
-	// 1.Create a new  list that looks like:
-	//   L->"a"->"b"->"c"->"d"
+  
+// 1.Create a new  list that looks like:
+//   L->"a"->"b"->"c"->"d" 
+	Node L = new Node("a");
+  Node L2 = new Node ("b");
+  L.setNext(L2); //set L's next node as L2
+  Node L3 = new Node ("c");
+  L2.setNext(L3); //set L2's next node as L3
+  Node L4 = new Node ("d");
+  L3.setNext(L4); //set L3's next node as L4
 
 	// 2. Write the code to insert an "x"
 	//    between the b and the c
+
+  Node lInsert = new Node("x"); //create new node
+  lInsert.setNext(L3); //set lInsert next node. toL3
+  L2.setNext(lInsert); //set L2 next node to lInsert
+  //checking
+  System.out.println(L2.getNext());
+  System.out.println((L2.getNext()).getNext());
+
+
 	// 3. Write the code to delete the c
-
-
+  lInsert.setNext(L4); //jump over c and connect lInsert to L4
+  System.out.println(lInsert.getNext());
+  
     }
 }
