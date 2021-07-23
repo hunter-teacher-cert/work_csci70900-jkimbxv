93a7c2a7dcf471d058633f629c514647ac9e0949
Jiyoon Kim <jkim@bronxvilleschool.org>
2021-07-23 11:24:48 -0400

    updated file structure

--- ds/tree/TreeNode.java
+++ ds/tree/TreeNode.java
@@ -6,38 +6,38 @@ public class TreeNode{
     private TreeNode left,right;
 
     public TreeNode(){
-	this(0);
-	left = null;
-	right = null;
+    	this(0);
+    	left = null;
+    	right = null;
     }
 
     public TreeNode(int data){
-	this.data = data;
-	left = null;
-	right = null;
+    	this.data = data;
+    	left = null;
+    	right = null;
     }
 
     public void setData(int data){
-	this.data = data;
+	     this.data = data;
     }
 
     public void setLeft(TreeNode left){
-	this.left = left;
+	     this.left = left;
     }
 
     public void setRight(TreeNode right){
-	this.right = right;
+	     this.right = right;
     }
 
     public int getData(){
-	return data;
+	     return data;
     }
 
     public TreeNode getLeft(){
-	return left;
+	     return left;
     }
 
     public TreeNode getRight(){
-	return right;
+	     return right;
     }
 }
