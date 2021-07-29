e5842115f7dc904dd85c107c9878665de92f7f3e
Jiyoon Kim <jkim@bronxvilleschool.org>
2021-07-29 14:33:48 -0400

    lesson 1 & lesson 2 uploaded

--- meth1/finalDeliverables/lesson1/brain.js
+++ meth1/finalDeliverables/lesson1/brain.js
@@ -1,9 +1,15 @@
 function getTixPrice(){
   var day = document.getElementById('dayDropDown').value;
+  //what does document.getElementById() mean?
+  //what goes in the parentheses?
+  //where is that ID stated?
   var age = document.getElementById('age').value;
+  //what does value mean?
   var discount = document.getElementById('coupon').value;
-  if (day == 'mon' || age <=5 || age >=65 || discount=='TIX5'){
+  //what am i accessing
+  if (day == 'mon' || age <=5 || age >=65 || discount=='TIX5'){ //how does this conditional work? how else could i implement this?
     document.getElementById('result').innerHTML = 'Price is $5';
+    //why did the innerHTML change from value?
   }
   else {
     document.getElementById('result').innerHTML = 'Price is $10';
